da94aedd3a5fd2e4c4753f35d61d6078e44db6e0
jinzbabz <jinsbabu03@gmail.com>
2026-02-13 12:29:42 +0530

    courses in the lms project

--- lms/accounts/views.py
+++ lms/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .models import Profile
 
 # Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        role = request.POST['role']
+
+        user = User.objects.create_user(username=username, password=password)
+        Profile.objects.create(user=user, role=role)
+        return redirect('accounts:login')
+    return render(request, 'accounts/register.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user :
+            login(request, user)
+            return redirect('core:dashboard')
+    return render(request, 'accounts/login.html')      
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
